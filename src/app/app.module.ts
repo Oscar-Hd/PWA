8a7c133c3281281f23b902a0506af8bf81ad3f8b
Oscar-Hd <oscarhernandezmar06@gmail.com>
2022-10-09 10:42:02 -0500

    paqueteria de pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { HeadComponent } from './Divisiones/head/head.component';
 import { BodyComponent } from './Divisiones/body/body.component';
 import { FooterComponent } from './Divisiones/footer/footer.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,13 @@ import { FooterComponent } from './Divisiones/footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
